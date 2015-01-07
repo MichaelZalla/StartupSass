a2ee55a50f8f3610554dffee958caf0112d6019a
Michael Zalla <michael.zalla@gmail.com>
2015-01-06 23:33:32 -0500

    Removed Markdown from Grunt 'copy' targets

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
 						expand: true,
 						flatten: false,
 						cwd: 'src/',
-						src: ['**/*.html','**/*.md'],
+						src: ['**/*.html'],
 						dest: 'dist'
 					},
 					{	// favicon
